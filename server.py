ec858c0cb1f097e18a2dc87adc2b6270fa6473f6
8wt4kzgf4k-ai <obale61@gmail.com>
2026-02-15 04:52:19 +0930

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,1 @@
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
 
-PORT = 8080
-
-with TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
-    print("Serving on port", PORT)
-    httpd.serve_forever()
